a3ed1318a76cfb4eda168087b1d0cba198f092df
hyeonseok <nohhyun03@naver.com>
2026-06-06 18:51:03 +0900

    init: create Spring Boot project

---
+++ src/main/java/com/spendiary/spendiary/SpenDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.spendiary.spendiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpenDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpenDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spendiary/spendiary/SpenDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendiary.spendiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpenDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
